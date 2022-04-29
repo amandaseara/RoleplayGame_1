a061efe45cec4fa432c76da888d327f49d976c3b
Seba-Quintana <SEBASTIAN.QUINTANA@correo.ucu.edu.uy>
2022-04-29 19:50:40 -0300

    espada

---
+++ src/Library/Espada.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace clases
+{
+    public class Espada
+    {
+        private int daño;
+        public int GetDaño()
+        {
+            return this.daño;
+        }
+        public Espada(int daño)
+        {
+            this.daño = 100;
+        }
+
+    }
+}
