2e5b3963a6a77706c68e8b4be9334232305451be
Seba-Quintana <SEBASTIAN.QUINTANA@correo.ucu.edu.uy>
2022-04-30 18:23:01 -0300

    hacha en vez de escudo en EquiparHacha

--- src/Library/Personajes/Enano.cs
+++ src/Library/Personajes/Enano.cs
@@ -82,9 +82,9 @@ namespace Roleplay
             // Si esta vacio, equipo item
             if (this.equipamiento[5].GetType() == 0.GetType())
             {
-                this.equipamiento[5] = escudo;
-                this.ataque += escudo.Daño;
-                this.defensa += escudo.Defensa;
+                this.equipamiento[5] = hacha;
+                this.ataque += hacha.Daño;
+                this.defensa += hacha.Defensa;
             }
         }
 
