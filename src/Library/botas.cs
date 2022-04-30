18e9da8d2bb04f1dd6eb19bc3a02552ac29c5e4a
amandaseara <amanda.seara@correo.ucu.edu.uy>
2022-04-30 01:21:39 -0300

    cambio botas

---
+++ src/Library/botas.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace Roleplay
+{
+    public class Botas
+    {
+        private int ataque = 0;
+        private int defensa = 500;
+
+        public int getDefensa()
+        {
+            return this.defensa;
+        }
+    }
+}
