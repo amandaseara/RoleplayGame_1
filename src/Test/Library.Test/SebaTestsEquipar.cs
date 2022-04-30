89df436c712c5ff5360b46b39c20a7da7db82a85
Seba-Quintana <SEBASTIAN.QUINTANA@correo.ucu.edu.uy>
2022-04-30 17:47:23 -0300

    tests equipar

--- src/Test/Library.Test/SebaTestsEquipar.cs
+++ src/Test/Library.Test/SebaTestsEquipar.cs
@@ -42,51 +42,234 @@ namespace Test.Library
         public void equiparElfo0()
         {
             elfoTest.EquiparYelmo(yelmoTest);
-            yelmoTest2 = new Yelmo(0,200);
+            Yelmo yelmoTest2 = new Yelmo(0, 400);
             elfoTest.EquiparYelmo(yelmoTest2);
-            Yelmo expected = yelmoTest;
-            Yelmo busqueda = elfoTest.;
-            Assert.AreEqual(expected, dañoYelmo);
+            Yelmo expected = yelmoTest2;
+            Yelmo busqueda = elfoTest.equipamiento[0];
+            Assert.AreEqual(expected, yelmoTest2);
         }
         [Test]
         public void equiparElfo1()
         {
-            Botas botas = new Botas(100, 0);
-            int dañoBotas = botas.Daño;
-            int expected = 100;
-            Assert.AreEqual(expected, dañoBotas);
+            elfoTest.EquiparPechera(pecheraTest);
+            Pechera pecheraTest2 = new Pechera(0, 400);
+            elfoTest.EquiparPechera(pecheraTest2);
+            Pechera expected = pecheraTest2;
+            Pechera busqueda = elfoTest.equipamiento[1];
+            Assert.AreEqual(expected, pecheraTest2);
         }
         [Test]
         public void equiparElfo2()
         {
-            Botas botas = new Botas(100, 0);
-            int dañoBotas = botas.Daño;
-            int expected = 100;
-            Assert.AreEqual(expected, dañoBotas);
+            elfoTest.EquiparGrebas(grebasTest);
+            Grebas grebasTest2 = new Grebas(0, 400);
+            elfoTest.EquiparGrebas(grebasTest2);
+            Grebas expected = grebasTest2;
+            Grebas busqueda = elfoTest.equipamiento[2];
+            Assert.AreEqual(expected, grebasTest2);
         }
         [Test]
         public void equiparElfo3()
         {
-            Botas botas = new Botas(100, 0);
-            int dañoBotas = botas.Daño;
-            int expected = 100;
-            Assert.AreEqual(expected, dañoBotas);
+            elfoTest.EquiparBotas(botasTest);
+            Botas botasTest2 = new Botas(0,400);
+            elfoTest.EquiparBotas(botasTest2);
+            Botas expected = botasTest2;
+            Botas busqueda = elfoTest.equipamiento[3];
+            Assert.AreEqual(expected, botasTest2);
         }
         [Test]
         public void equiparElfo4()
         {
-            Botas botas = new Botas(100, 0);
-            int dañoBotas = botas.Daño;
-            int expected = 100;
-            Assert.AreEqual(expected, dañoBotas);
+            elfoTest.EquiparEspada(espadaTest);
+            Espada espadaTest2 = new Espada(100,200);
+            elfoTest.EquiparEspada(espadaTest2);
+            Espada expected = espadaTest2;
+            Espada busqueda = elfoTest.equipamiento[4];
+            Assert.AreEqual(expected, espadaTest2);
         }
         [Test]
         public void equiparElfo5()
         {
-            Botas botas = new Botas(100, 0);
-            int dañoBotas = botas.Daño;
-            int expected = 100;
-            Assert.AreEqual(expected, dañoBotas);
+            elfoTest.EquiparArco(arcoTest);
+            Arco arcoTest2 = new Arco(100,200);
+            elfoTest.EquiparArco(arcoTest2);
+            Arco expected = arcoTest2;
+            Arco busqueda = elfoTest.equipamiento[5];
+            Assert.AreEqual(expected, arcoTest2);
         }
+
+
+
+        public void equiparEnano0()
+        {
+            enanoTest.EquiparYelmo(yelmoTest);
+            Yelmo yelmoTest2 = new Yelmo(0, 400);
+            enanoTest.EquiparYelmo(yelmoTest2);
+            Yelmo expected = yelmoTest2;
+            Yelmo busqueda = enanoTest.equipamiento[0];
+            Assert.AreEqual(expected, yelmoTest2);
+        }
+        [Test]
+        public void equiparEnano1()
+        {
+            enanoTest.EquiparPechera(pecheraTest);
+            Pechera pecheraTest2 = new Pechera(0, 400);
+            enanoTest.EquiparPechera(pecheraTest2);
+            Pechera expected = pecheraTest2;
+            Pechera busqueda = enanoTest.equipamiento[1];
+            Assert.AreEqual(expected, pecheraTest2);
+        }
+        [Test]
+        public void equiparEnano2()
+        {
+            enanoTest.EquiparGrebas(grebasTest);
+            Grebas grebasTest2 = new Grebas(0, 400);
+            enanoTest.EquiparGrebas(grebasTest2);
+            Grebas expected = grebasTest2;
+            Grebas busqueda = enanoTest.equipamiento[2];
+            Assert.AreEqual(expected, grebasTest2);
+        }
+        [Test]
+        public void equiparEnano3()
+        {
+            enanoTest.EquiparBotas(botasTest);
+            Botas botasTest2 = new Botas(0,400);
+            enanoTest.EquiparBotas(botasTest2);
+            Botas expected = botasTest2;
+            Botas busqueda = enanoTest.equipamiento[3];
+            Assert.AreEqual(expected, botasTest2);
+        }
+        [Test]
+        public void equiparEnano4()
+        {
+            enanoTest.EquiparEspada(espadaTest);
+            Espada espadaTest2 = new Espada(100,200);
+            enanoTest.EquiparEspada(espadaTest2);
+            Espada expected = espadaTest2;
+            Espada busqueda = enanoTest.equipamiento[4];
+            Assert.AreEqual(expected, espadaTest2);
+        }
+        [Test]
+        public void equiparEnano5()
+        {
+            enanoTest.EquiparHacha(hachaTest);
+            Hacha hachaTest2 = new Hacha(100,200);
+            enanoTest.EquiparHacha(hachaTest2);
+            Hacha expected = hachaTest2;
+            Hacha busqueda = enanoTest.equipamiento[5];
+            Assert.AreEqual(expected, hachaTest2);
+        }
+
+        public void equiparHumano0()
+        {
+            humanoTest.EquiparYelmo(yelmoTest);
+            Yelmo yelmoTest2 = new Yelmo(0, 400);
+            humanoTest.EquiparYelmo(yelmoTest2);
+            Yelmo expected = yelmoTest2;
+            Yelmo busqueda = humanoTest.equipamiento[0];
+            Assert.AreEqual(expected, yelmoTest2);
+        }
+        [Test]
+        public void equiparHumano1()
+        {
+            humanoTest.EquiparPechera(pecheraTest);
+            Pechera pecheraTest2 = new Pechera(0, 400);
+            humanoTest.EquiparPechera(pecheraTest2);
+            Pechera expected = pecheraTest2;
+            Pechera busqueda = humanoTest.equipamiento[1];
+            Assert.AreEqual(expected, pecheraTest2);
+        }
+        [Test]
+        public void equiparHumano2()
+        {
+            humanoTest.EquiparGrebas(grebasTest);
+            Grebas grebasTest2 = new Grebas(0, 400);
+            humanoTest.EquiparGrebas(grebasTest2);
+            Grebas expected = grebasTest2;
+            Grebas busqueda = humanoTest.equipamiento[2];
+            Assert.AreEqual(expected, grebasTest2);
+        }
+        [Test]
+        public void equiparHumano3()
+        {
+            humanoTest.EquiparBotas(botasTest);
+            Botas botasTest2 = new Botas(0,400);
+            humanoTest.EquiparBotas(botasTest2);
+            Botas expected = botasTest2;
+            Botas busqueda = humanoTest.equipamiento[3];
+            Assert.AreEqual(expected, botasTest2);
+        }
+        [Test]
+        public void equiparHumano4()
+        {
+            humanoTest.EquiparEspada(espadaTest);
+            Espada espadaTest2 = new Espada(100,200);
+            humanoTest.EquiparEspada(espadaTest2);
+            Espada expected = espadaTest2;
+            Espada busqueda = humanoTest.equipamiento[4];
+            Assert.AreEqual(expected, espadaTest2);
+        }
+        [Test]
+        public void equiparHumano5()
+        {
+            humanoTest.EquiparEscudo(escudoTest);
+            Escudo escudoTest2 = new Escudo(100,200);
+            humanoTest.EquiparEscudo(escudoTest2);
+            Escudo expected = escudoTest2;
+            Escudo busqueda = humanoTest.equipamiento[5];
+            Assert.AreEqual(expected, escudoTest2);
+        }
+
+        public void equiparHechicero0()
+        {
+            hechiceroTest.EquiparYelmo(yelmoTest);
+            Yelmo yelmoTest2 = new Yelmo(0, 400);
+            hechiceroTest.EquiparYelmo(yelmoTest2);
+            Yelmo expected = yelmoTest2;
+            Yelmo busqueda = hechiceroTest.equipamiento[0];
+            Assert.AreEqual(expected, yelmoTest2);
+        }
+        [Test]
+        public void equiparHechicero1()
+        {
+            hechiceroTest.EquiparPechera(pecheraTest);
+            Pechera pecheraTest2 = new Pechera(0, 400);
+            hechiceroTest.EquiparPechera(pecheraTest2);
+            Pechera expected = pecheraTest2;
+            Pechera busqueda = hechiceroTest.equipamiento[1];
+            Assert.AreEqual(expected, pecheraTest2);
+        }
+        [Test]
+        public void equiparHechicero2()
+        {
+            hechiceroTest.EquiparGrebas(grebasTest);
+            Grebas grebasTest2 = new Grebas(0, 400);
+            hechiceroTest.EquiparGrebas(grebasTest2);
+            Grebas expected = grebasTest2;
+            Grebas busqueda = hechiceroTest.equipamiento[2];
+            Assert.AreEqual(expected, grebasTest2);
+        }
+        [Test]
+        public void equiparHechicero3()
+        {
+            hechiceroTest.EquiparBotas(botasTest);
+            Botas botasTest2 = new Botas(0,400);
+            hechiceroTest.EquiparBotas(botasTest2);
+            Botas expected = botasTest2;
+            Botas busqueda = hechiceroTest.equipamiento[3];
+            Assert.AreEqual(expected, botasTest2);
+        }
+        [Test]
+        public void equiparHechicero4()
+        {
+            hechiceroTest.EquiparBaculo(baculoTest);
+            Baculo baculoTest2 = new baculo(100,200);
+            hechiceroTest.EquiparBaculo(baculoTest2);
+            Baculo expected = baculoTest2;
+            Baculo busqueda = hechiceroTest.equipamiento[4];
+            Assert.AreEqual(expected, baculoTest2);
+        }
+
     }
 }
